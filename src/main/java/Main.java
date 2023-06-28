4281f9478e9c42baac01fbff4b45c33296ee0c7e
Angela Kim <aangelakkim@gmail.com>
2023-06-28 17:14:18 -0400

    Lab completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+    // Ask for user's name
+    String response = "";
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is your name: ");
+    response = in.nextLine();
 
+    // Greet user if his/her name is Bob or Alice
+    if(response.equalsIgnoreCase("Bob") || response.equalsIgnoreCase("Alice")) {
+        System.out.println("Greetings " + response + "!");
+    }
+    else {  // Else, display user name
+        System.out.println("Your name is " + response + ".");
+    }
     }
 }
